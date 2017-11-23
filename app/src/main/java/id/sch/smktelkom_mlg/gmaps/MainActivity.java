53219a94fa78c7b7bcc56bc39d17d57a697de4c1
nabilaalissa24 <9d.nabilaalissa@gmail.com>
2017-11-23 18:32:08 +0700

    GMapsAPI

---
+++ app/src/main/java/id/sch/smktelkom_mlg/gmaps/MainActivity.java
@@ -0,0 +1,33 @@
+package id.sch.smktelkom_mlg.gmaps;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
+
+public class MainActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        StreetViewPanoramaFragment streetViewPanoramaFragment = (StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+
+    }
+
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+        panorama.setPosition(new LatLng(-6.921932, 107.607639));
+        panorama.setStreetNamesEnabled(false);
+        StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera.Builder()
+                .bearing(180)
+                .build();
+        panorama.animateTo(camera, 10000);
+
+    }
+}
